14fd957583d4b19587ca9eaecf9c46246a12e708
Dominika Nowak <dominika.nowak@student.put.poznan.pl>
2023-11-08 22:49:30 +0100

    dodanie zapisywania do pliku

--- main.py
+++ main.py
@@ -9,9 +9,19 @@ def readFasta(file_path):
             sequences.append(str(record.seq))
     return sequences
 
+def saveToFile(outputFile, score, alignments):
+    with open(outputFile, "w") as file:
+        file.write(f"Score: {score}\n")
+        file.write("Alignments:\n")
+        for i, alignment in enumerate(alignments, 1):
+            file.write(f"\nAlignment {i}:\n")
+            file.write(f"Alignment 1: {alignment[0]}\n")
+            file.write(f"Alignment 2: {alignment[1]}\n")
+
 def main():
     parser = argparse.ArgumentParser(description="Needleman-Wunsch Algorithm")
     parser.add_argument("--f", dest="fastaFile", required=True, help="Path to the fasta file containing sequences")
+    parser.add_argument("--o", dest="outputFile", default="output.txt", help="Path to the output file")
 
     args = parser.parse_args()
 
@@ -32,5 +42,10 @@ def main():
         print("Alignment 1:", alignment[0])
         print("Alignment 2:", alignment[1])
         print()
+
+    saveToFile(args.outputFile, score, alignments)
+    print(f"Results saved to {args.outputFile}")
+
+
 if __name__ == "__main__":
     main()
